8476d81c00cd131f7fe0d9a641ffc2a7d6721b84
Lukas Nehrke <lukas.nehrke@gmail.com>
2021-03-11 10:16:24 +0100

    Add service worker

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,15 @@
 const RemarkHTML = require('remark-html');
+const {GenerateSW} = require('workbox-webpack-plugin');
 
 module.exports = {
   chainWebpack: config => {
     config.module.rule('markdown').test(/\.md$/)
       .use('html-loader').loader('html-loader').end()
       .use('remark-loader').loader('remark-loader').options({ remarkOptions: { plugins: [RemarkHTML] } }).end()
+  },
+  configureWebpack: {
+    plugins: [
+      new GenerateSW()
+    ]
   }
 }
