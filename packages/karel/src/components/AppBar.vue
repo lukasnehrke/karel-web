58ed07269356f4feb9f172938b0114d817c3be51
Lukas Nehrke <lukasnehrke@protonmail.com>
2021-03-30 15:04:27 +0200

    Add github link to app bar

--- packages/karel/src/components/AppBar.vue
+++ packages/karel/src/components/AppBar.vue
@@ -13,7 +13,15 @@ export default defineComponent({
         <span class="app-bar__brand-title">Karel</span>
       </router-link>
       <div class="app-bar__nav"></div>
-      <div class="app-bar__actions" aria-label="Created with Love">❤️</div>
+      <a class="app-bar__actions" href="https://github.com/evorto/karel" target="_blank" rel="noopener">
+        <span>GitHub</span>
+        <svg style="width: 18px; height: 18px; margin-left: 6px" viewBox="0 0 24 24">
+          <path
+            fill="currentColor"
+            d="M14,3V5H17.59L7.76,14.83L9.17,16.24L19,6.41V10H21V3M19,19H5V5H12V3H5C3.89,3 3,3.9 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V12H19V19Z"
+          />
+        </svg>
+      </a>
     </div>
   </header>
 </template>
